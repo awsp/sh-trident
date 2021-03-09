bf706a0c1033c1e03e4e91e924dce0690ac20f5c
awsp <anthonyspwu@gmail.com>
2021-03-09 01:05:34 -0700

    Sort desc

--- g-demolition/components/subscription/FeedList.js
+++ g-demolition/components/subscription/FeedList.js
@@ -2,7 +2,7 @@ import {Tag} from "@blueprintjs/core";
 
 const FeedList = ({feeds}) => {
   return <>
-    {feeds.sort((a, b) => new Date(a.pubDate) - new Date(b.pubDate)).map(feed => (
+    {feeds.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate)).map(feed => (
       <p key={`feed-${feed.id}`}>
         <Tag intent="primary">{feed.pubDate}</Tag>&nbsp;
         <a href={feed.enclosure} target="_blank">{feed.title}</a>
